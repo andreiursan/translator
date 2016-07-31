684b1cf704383c57e83b4185f2be04c1b263c67b
Andrei Ursan <hello@andreiursan.com>
2016-07-31 20:56:50 +0200

    added a description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject translator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Library designed to parse translation files."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
