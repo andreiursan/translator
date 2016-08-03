5d0f27a47361e67cadd2a5bc7ef340f3dff08c50
Andrei Ursan <hello@andreiursan.com>
2016-08-03 22:51:10 +0200

    writing a first test

--- test/translator/core_test.clj
+++ test/translator/core_test.clj
@@ -1,7 +1,10 @@
 (ns translator.core-test
   (:require [clojure.test :refer :all]
-            [translator.core :refer :all]))
+            [translator.core :refer :all]
+            [clojure.java.io :as io]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest transforming-translations
+  (testing "reading strings files"
+    (is (= (translation->clojure :strings (io/reader "./fixtures/translations/strings/en.strings"))
+           {:en {"hello" "Hello!"
+                 "click_button" "Click"}}))))
