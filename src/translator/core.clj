e7d319ca44d61a669df268595d2bf0e0bc327992
Andrei Ursan <hello@andreiursan.com>
2016-08-19 22:20:45 +0200

    implemented, clojure transformation for a strings translation directory

--- src/translator/core.clj
+++ src/translator/core.clj
@@ -3,7 +3,6 @@
             [clojure.java.io :as io]
             [clojure.string :refer [split]]))
 
-
 (defn translation->clojure
   "parses translation files"
   [translation-type file]
@@ -13,3 +12,8 @@
         parsed-lines (map strings/parser lines)]
     {locale (into {} (map vec parsed-lines))}))
 
+(defn translations->clojure
+  "parses translation files"
+  [translation-type files]
+  (let [only-files (filter #(not (.isDirectory %)) files)]
+    (into {} (map #(translation->clojure :strings %) only-files))))
