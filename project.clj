c6e86bb949bda6b437699b66d9305c2062329a04
Andrei Ursan <hello@andreiursan.com>
2016-09-01 12:01:23 +0200

    update deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
-                 [instaparse "1.4.2"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
+                 [instaparse "1.4.3"]
                  [io.forward/yaml "1.0.4"]]
 
   :profiles
